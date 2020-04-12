6fb6a4c39bbf396c2cd671537823bb9c2ed6212a
erwan-joly <erwan.joly@pushpay.com>
2020-04-12 16:45:31 +1200

    Initial commit

---
+++ src/NosCore.PathFinder/Heuristic.cs
@@ -0,0 +1,25 @@
+﻿//  __  _  __    __   ___ __  ___ ___
+// |  \| |/__\ /' _/ / _//__\| _ \ __|
+// | | ' | \/ |`._`.| \_| \/ | v / _|
+// |_|\__|\__/ |___/ \__/\__/|_|_\___|
+// -----------------------------------
+
+using System;
+using NosCore.PathFinder.Interfaces;
+
+namespace NosCore.PathFinder
+{
+    public class OctileDistance : IDistance
+    {
+        public readonly double Sqrt2 = Math.Sqrt(2);
+
+        public double GetDistance(MapCell fromCell, MapCell toCell)
+        {
+            var iDx = Math.Abs(fromCell.X - toCell.X);
+            var iDy = Math.Abs(fromCell.Y - toCell.Y);
+            var min = Math.Min(iDx, iDy);
+            var max = Math.Max(iDx, iDy);
+            return min * Sqrt2 + max - min;
+        }
+    }
+}
---
+++ src/NosCore.PathFinder/MapCell.cs
@@ -0,0 +1,14 @@
+﻿//  __  _  __    __   ___ __  ___ ___
+// |  \| |/__\ /' _/ / _//__\| _ \ __|
+// | | ' | \/ |`._`.| \_| \/ | v / _|
+// |_|\__|\__/ |___/ \__/\__/|_|_\___|
+// -----------------------------------
+
+namespace NosCore.PathFinder
+{
+    public class MapCell
+    {
+        public short X { get; set; }
+        public short Y { get; set; }
+    }
+}
